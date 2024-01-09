da0409ab298e3193d38aaeab493779414607e571
250391Ivan <25ivan.pillado@gmail.com>
2024-01-09 12:33:05 -0600

    Add .gitignore and install dotenv

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 
 const express = require("express");
 const server = express();
+require("dotenv").config();
 
 server.get("/", (req, res) => {
   res.send("Method  get  for testing");
 });
 
 //server.listen(8085);
-const PORT = 8085;
+const PORT = process.env.PORT;
 const Myserver = server.listen(PORT, () => {
   console.log("Server  Run ");
   console.log(`App open  in Port :: ${PORT}`);
