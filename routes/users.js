3f32ee1b0e334baa3179001acd8b6edd03cc2e8a
250391Ivan <25ivan.pillado@gmail.com>
2024-01-12 11:40:09 -0600

    Add middleware  for  validate  fields  on petitions post for  body  info

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 //Make routes  for endpoints CRUD
 const { Router } = require("express");
 const router = Router();
+const { check } = require("express-validator");
+const { FieldsValidate } = require("../middleware/validateFields");
 const {
   AllUsers,
   MakeUsers,
@@ -10,13 +12,34 @@ const {
 } = require("../controllers/usersController");
 
 //Create  user
-router.post("/MakeUser", MakeUsers);
+router.post(
+  "/MakeUser",
+  [
+    check("Email", "Email is  mandatory").isEmail(),
+    check("IdRol", "IdRol Is  necesary ").not().isEmpty(),
+    check("FirstName", "FirstName Is  necesary ").not().isEmpty(),
+    check("LastName", "LastName Is  necesary ").not().isEmpty(),
+    FieldsValidate,
+  ],
+  MakeUsers
+);
 //Read  user
 router.get("/AllUsers", AllUsers);
 //Read  specific  User
 router.get("/UniqueUser/:id", OnlyUser);
 //Update  user
-router.post("/UpdateUser", UpdateDatUser);
+router.post(
+  "/UpdateUser",
+  [
+    check("IdUser", "IdUser Is  necesary ").not().isEmpty(),
+    check("IdRol", "IdRol Is  necesary ").not().isEmpty(),
+    check("FirstName", "FirstName Is  necesary ").not().isEmpty(),
+    check("LastName", "LastName Is  necesary ").not().isEmpty(),
+    check("Email", "Email is  mandatory").isEmail(),
+    FieldsValidate,
+  ],
+  UpdateDatUser
+);
 //Delete User
 router.delete("/CleanUser", DeleteUser);
 
